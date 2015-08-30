3cad8d5859c905f3b1d9a0501b8318d89c1cfc5f
Jimmy <morzaria.jimmy@gmail.com>
2015-08-30 12:25:47 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
-Rails.application.routes.draw do
-  get 'static_pages/home'
+SampleApp::Application.routes.draw do
+  get 'users/new'
 
-  get 'static_pages/help'
+  #get 'static_pages/home'
 
-  get "static_pages/about"
+  #get 'static_pages/help'
+
+  #get "static_pages/about"
+
+  #get "static_pages/contact"
+
+  root to: 'static_pages#home', :via => [:get, :post]
+
+  match 'static_pages/home', to: 'static_pages#home', :via => [:get, :post]
+
+  match '/help', to: 'static_pages#help', :via => [:get, :post]
+
+  match '/about', to: 'static_pages#about', :via => [:get, :post]
+
+  match '/contact', to: 'static_pages#contact', :via => [:get, :post]
+
+  match '/signup', to: 'users#new', :via => [:get, :post]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,40 +1,31 @@
 require 'rails_helper'
 	describe "Static pages" do
+				
+		subject { page }
+		
 		describe "Home page" do
-			it "should have the h1 'Sample App'" do
-				visit '/static_pages/home'
-				page.should have_selector('h1', :text => 'Sample App')
-			end
-			it "should have the title 'Home'" do
-				visit '/static_pages/home'
-				page.should have_selector('title', 
-					:text => "Ruby on Rails Tutorial Sample App | Home", 
-					visible: false)
-			end	
+			before { visit root_path }
+
+			it { should have_selector('h1', text: 'awesomeness') }
+			it { should have_selector('title', text: full_title(''), 
+					visible: false) }
+			it { should_not have_selector('title', text: "| Home", 
+					visible: false) }
 		end
 		describe "Help page" do
-			it "should have the h1 'Help'" do
-				visit '/static_pages/help'
-				page.should have_selector('h1', :text => 'Help')
-			end
-			it "should have the title 'Help" do
-				visit '/static_pages/help'
-				page.should have_selector('title', 
-					:text => "Ruby on Rails Tutorial Sample App | Help",
-					visible: false)
-			end	
+			before { visit help_path }			
+			it { should have_selector('h1', text: 'Help') }
+			it { should have_selector('title', text: full_title('Help'), 
+					visible: false) }	
+		end	
 		describe "About page" do
-			it "should have the h1 'About Us'" do
-				visit '/static_pages/about'
-				page.should have_selector('h1', :text => 'About Us')
-			end
-			it "should have the title 'About Us" do
-				visit '/static_pages/about'
-				page.should have_selector('title', 
-					:text => "Ruby on Rails Tutorial Sample App | About Us",
-					visible: false)
-			end	
+			before { visit about_path }
+			it { should have_selector('h1', text: 'About') }
+			it { should have_selector('title', text: full_title('About Us'), visible: false) }
+		end
+		describe "Contact page" do
+			before { visit contact_path }
+			it { should have_selector('h1', text: 'Contact') }
+			it { should have_selector('title', text: full_title('Contact'), visible: false) }
 		end
 	end
-
-end
